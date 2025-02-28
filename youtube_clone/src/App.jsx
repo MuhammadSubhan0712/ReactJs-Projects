129897a6a152b4ef1925f33c480b2e51ef3666ba
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-03-01 00:46:36 +0500

    Youtube-Clone

--- youtube_clone/src/App.jsx
+++ youtube_clone/src/App.jsx
@@ -10,7 +10,6 @@ const App = () => {
   return (
     <div>
       <Navbar setSidebar={setSidebar} />
-
       <Routes>
         <Route path="/" element={<Home sidebar={sidebar} />} />
         <Route path="/video/:categoryId/:videoId" element={<Video />} />
