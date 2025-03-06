861d7ef5974b38e78854ec9a932678b8d0bddcb5
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-03-07 02:37:30 +0500

    Working..

---
+++ personal-portfolio/src/Components/Hero/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='relative overflow-hidden min-h-[660px] flex flex-col'>
+    <div className='md:h-[720px] h-[960px] md:w-[640px] w-[880-px] right-0 bg-gradient-to-r from-orange-600
+    via-yellow-500 to-red-400 absolute rounded-full transform rotate-12 -top-20 shadow-[0_0_40px_rgb(255, 165 , 0 , 0.7)]'>
+
+    </div>
+
+    </div>
+  )
+}
+
+export default Hero
