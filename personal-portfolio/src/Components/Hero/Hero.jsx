712d8f7ef7dd41d0efc8449a8b4ccf5854c23be8
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-03-18 01:04:48 +0500

    Almost Done.

--- personal-portfolio/src/Components/Hero/Hero.jsx
+++ personal-portfolio/src/Components/Hero/Hero.jsx
@@ -1,14 +1,50 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
+import hi from "../../assets/hi.png";
+import img from "../../assets/Muhammad Subhan Khan.png";
 
 const Hero = () => {
   return (
     <div className="relative overflow-hidden min-h-[660px] flex flex-col">
       <div
         className="md:h-[720px] h-[960px] md:w-[640px] w-[880-px] right-0 bg-gradient-to-r from-orange-600
-    via-yellow-500 to-red-400 absolute rounded-full transform rotate-12 -top-20 shadow-[0_0_40px_rgb(255, 165 , 0 , 0.7)]">
-    </div>
-    <Navbar/>
+    via-yellow-500 to-red-400 absolute rounded-full transform rotate-12 -top-20 shadow-[0_0_40px_rgb(255, 165 , 0 , 0.7)]"></div>
+      <Navbar />
+
+      <section className="text-white body-font z-10">
+        <div className="container mx-auto px-5 py-24 md:flex-row flex-col items-center">
+          <div
+            className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left
+    mb-16 md:mb-0 items-center text-center relative">
+            <img
+              src={hi}
+              className="absolute top-[350px] left-44 md:top-[-30px] md:left-[450px]
+    transform -translate-x-1/2 -translate-y-1/2 w-20 h-20"
+            />
+            <h1 className="title-font sm:text-4xl text-4xl mb-4 font-bold text-white">
+              Hi! I'm Muhammad Subhan Khan{" "}
+            </h1>
+            <p className="mb-8 leading-relaxed">
+              a tech enthusiast who thrives on Full-Stack Software development.
+              I'm passionate about crafting code that brings joy, staying ahead
+              of the curve with the latest tech innovations, and creating
+              projects that shake things up. At the moment, I'm fully immersed
+              in the metaverse, generative AI, and web 3.0.
+            </p>
+            <div className="flex justify-center">
+            <a href={} download>
+              <button className="inline-flex text-white bg-orange-500 border-0 py-2 px-8
+              focus:outline-none hover:bg-orange-600 hover:shadow-[0_0_4px_rgb(255,165,0,0.7)] rounded-full text-lg">
+              Download CV
+              </button>
+            </a>
+            </div>
+          </div>
+          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <img src={img} alt="hero" className="object-cover object-center rounded-full w-80 h-80"/>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
--- personal-portfolio/src/Components/Navbar/Navbar.jsx
+++ personal-portfolio/src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Navbar = () => {
+
   const listNavbar = [
     { name: "Home", link: "#home" },
     { name: "Skills", link: "#skills" },
@@ -8,8 +9,9 @@ const Navbar = () => {
     { name: "Language", link: "#language" },
     { name: "Projects", link: "#projects" },
   ];
+
   return (
-    <header className="text-zinc-600 body-font z-10">
+    <header className="text-gray-600 body-font z-10">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <a className="flex title-font font-medium items-center text-zinc-900 mb-4 md:mb-0">
           <span className="ml-3 text-3xl font-bold text-white">
@@ -28,7 +30,7 @@ const Navbar = () => {
         </nav>
         <button
           className="inline-flex item-center py-1 px-7 focus:outline-none text-base text-white 
-mt-4 md:mt-0 border-white hover:border-yellow-300 hover:text-zinc-900">
+          mt-4 md:mt-0 border border-white hover:border-yellow-300 hover:text-zinc-900">
           <a href="#contact">Contact me</a>
         </button>
       </div>
