d6a8546f289be7b929e64d7aa78a410bfbe0a536
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-04-05 12:26:26 +0500

    Working..

--- ecommerce/src/Pages/Products.jsx
+++ ecommerce/src/Pages/Products.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useDispatch , useSelector } from 'react-redux';
-import { increment } from '../Config/reducer/cartSlice';
+import { addItem } from '../Config/reducer/cartSlice';
 import { Loader } from 'lucide-react';
 const Products = () => {
   const [products, setProducts] = useState(null);
@@ -65,7 +65,7 @@ const Products = () => {
                   <span className="text-3xl font-bold text-gray-900 dark:text-white">
                     ${item.price}
                   </span>
-                  <button onClick={() => dispatch(increment())}
+                  <button onClick={() => dispatch(addItem())}
                     className="text-white bg-blue-500 hover:bg-blue-900 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 transition-transform duration-300 ease-in-out"
                   >
                     Add to cart
