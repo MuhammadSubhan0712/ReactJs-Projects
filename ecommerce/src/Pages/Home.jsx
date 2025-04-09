7682c61b2cb268761c2af3e691290a6b16eb944a
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-04-09 14:20:52 +0500

    Working..

--- ecommerce/src/Pages/Home.jsx
+++ ecommerce/src/Pages/Home.jsx
@@ -3,12 +3,54 @@ import Cards from "../Components/Cards";
 import Carousel from "../Components/Carousel";
 
 export const Home = () => {
+
+  const categories = [
+    {name: "Electronics" , image: "/assets/categories/electronics.jpg"},
+    {name: "Fashion" , image: "/assets/categories/fashion.jpg"},
+    {name: "Home & Kitchen" , image: "/assets/categories/home.jpg"},
+    {name: "Beauty" , image: "/assets/categories/beauty.jpg"},
+  ]
+
+  // Promtional Banners:
+  const promotion = [
+    {
+      title: "Summer Sale",
+      subtitle: "Up to 50% off",
+      image: "/asssets/promos/summer-sale.jpg",
+      cta: "Shop Now",
+    },
+
+    {
+      title: "Summer Sale",
+      subtitle: "Up to 50% off",
+      image: "/asssets/promos/summer-sale.jpg",
+      cta: "Shop Now",
+    },
+
+    {
+      title: "Summer Sale",
+      subtitle: "Up to 50% off",
+      image: "/asssets/promos/summer-sale.jpg",
+      cta: "Shop Now",
+    },
+
+    {
+      title: "Summer Sale",
+      subtitle: "Up to 50% off",
+      image: "/asssets/promos/summer-sale.jpg",
+      cta: "Shop Now",
+    },
+  ]
   return (
     <>
+
       <div className="bg-gradient-to-r from-blue-200 to bg-blue-300">
         <div>
           <Carousel />
         </div>
+
+
+        
 
         <div>
           <Cards />
