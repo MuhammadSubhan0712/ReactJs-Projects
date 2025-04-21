caf3fa0f930ddfbaf6d35aa506d1c99c9992090f
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-04-22 01:50:48 +0500

    Working..

--- Online-Bus-Booking/src/Pages/Hero/Hero.jsx
+++ Online-Bus-Booking/src/Pages/Hero/Hero.jsx
@@ -1,40 +1,49 @@
-import React from 'react'
+import React from "react";
 
-import {motion} from 'framer-motion';
-import RootLayout from '../../Layout/RootLayout';
-
+import { motion } from "framer-motion";
+import RootLayout from "../../Layout/RootLayout";
+import Search from "../Search/Search";
 
 const Hero = () => {
-
-    const variants = {
-    hidden :{opacity: 0 , y: -800} ,
-    visible: {opacity: 1 , y: 0}
-}
-
+  const variants = {
+    hidden: { opacity: 0, y: -800 },
+    visible: { opacity: 1, y: 0 },
+  };
 
   return (
-    <motion.div className='w-full flex-1 h-screen bg-[url("./assets/herobg.png")] bg-cover bg-no-repeat bg-top relative'
-    initial="hidden"
-    animate="visible"
-    variants={variants}
-    transition={{ duration: 0.85, ease: "easeInOut" }}>
+    <motion.div
+      className='w-full flex-1 h-screen bg-[url("./assets/herobg.png")] bg-cover bg-no-repeat bg-top relative'
+      initial="hidden"
+      animate="visible"
+      variants={variants}
+      transition={{ duration: 0.85, ease: "easeInOut" }}>
+      <RootLayout className="absolute top-0 left-0 w-full h-full py-[9ch] bg-gradient-to-b from bg-neutral-50/70 via-neutral-50/15 to-neutral-50/5 flex items-center justify-start text-center flex-col gap-9">
+        {/* Title Section */}
+        <div className="space-y-2">
+          <motion.p
+            initial={{ opacity: 0, y: -800 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -800 }}
+            transition={{ duration: 0.2, ease: "easeOut" }}
+            className="text-lg text-neutral-500 font-medium">
+            Get your bus tickets
+          </motion.p>
 
-    <RootLayout className="absolute top-0 left-0 w-full h-full py-[9ch] bg-gradient-to-b from bg-neutral-50/70 via-neutral-50/15 to-neutral-50/5 flex items-center justify-start text-center flex-col gap-9">
+          <motion.h1
+            initial={{ opacity: 0, y: -800 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -800 }}
+            transition={{ duration: 1.85, ease: "easeOut" }}
+            className="text-lg text-neutral-800 font-bold capitalize">
+            Find best bus for you!
+          </motion.h1>
+        </div>
 
-     {/* Title Section */}
-     <div className="space-y-2">
-        <motion.div 
-        initial={{ opacity: 0 , y: -800 }}
-        animate={{ opacity: 1 , y: 0 }}
-        exit={{ opacity: 0 , y: -800 }}
-        transition={{ duration: 0.2 , ease: "easeOut" }}
-        className='text-lg text-neutral-500 font-medium'>
-        Get your bus tickets
-        </motion.div>
-     </div>
-    </RootLayout>
+        {/* Search section */}
+        <Search />
+      </RootLayout>
     </motion.div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
