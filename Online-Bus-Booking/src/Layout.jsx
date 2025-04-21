b5c800980feded64af21e893e52d03fbc37e00df
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-04-22 00:55:16 +0500

    Working..

---
+++ Online-Bus-Booking/src/Layout.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react'
+
+const Layout = ({ children , className }) => {
+
+    // Automatically scroll to top when we navigate to another page. or new page:
+    useEffect(()=>{
+       window.scrollTo(0, 0)
+    },[])
+  return (
+    <div className={`w-full lg:px-24 md:px-16 sm:px-7 px-4 ${className}`}>
+        {children}
+    </div>
+  )
+}
+
+export default Layout
