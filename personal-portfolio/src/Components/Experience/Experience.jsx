2a07f4c1a5d650b214ba2cb196da136a3be80718
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-04-29 20:11:31 +0500

    Woking..

--- personal-portfolio/src/Components/Experience/Experience.jsx
+++ personal-portfolio/src/Components/Experience/Experience.jsx
@@ -11,15 +11,14 @@ const Experience = () => {
 
         <div className="h-96 w-96 bg-graident-to-br from-[#ff910a] blur-2xl invisible opacity-40 rounded-full"></div>
       </div>
-     
 
-      <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center"> 
+      <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
         <div
           data-aos="fade-right"
           data-aos-delay="400"
           className="relative lg:max-w-lg lg:w-full md:w-1/2 w-5/6 md:mb-0">
-        <div
-          className="md:h-[450px] h-[400px] md:w-[300px] w-[300px] md:right-36 bg-gradient-to-r from-orange-600 via-yellow-500 to bg-red-400
+          <div
+            className="md:h-[450px] h-[400px] md:w-[300px] w-[300px] md:right-36 bg-gradient-to-r from-orange-600 via-yellow-500 to bg-red-400
     absolute rounded-md transform rotate-3 -top-8 shadow-[0_0_40px_rgba(255,165,0,0.7)] z-0"></div>
           <img
             src={img_experience}
@@ -34,22 +33,14 @@ const Experience = () => {
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-orange-500">
             Experience
           </h1>
-          <h2 className="title-font font-bold text-white sm:text-2xl">
-            2024 :
-          </h2>
-          <p className="mb-8 text-white leading-relaxed">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-            magnam beatae voluptatem veniam inventore necessitatibus nulla omnis
-            itaque dolores distinctio.
-          </p>
 
-          <h2 className="title-font font-bold text-white sm:text-2xl">
-            2025 :
-          </h2>
           <p className="mb-8 text-white leading-relaxed">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-            magnam beatae voluptatem veniam inventore necessitatibus nulla omnis
-            itaque dolores distinctio.
+            During my academic career, I completed two internships, refining my
+            problem-solving and agile development skills. Additionally, I’ve
+            strengthened my expertise through personal and academic projects,
+            implementing RESTful APIs, version control (Git/GitHub), and modern
+            development best practices. My goal is to write efficient,
+            maintainable code while delivering seamless user experiences.
           </p>
         </div>
       </div>
