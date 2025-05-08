a346ba9db0685498467ba95878b4d60ab2b00ab4
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-05-09 01:47:05 +0500

    Working..

--- my-portfolio/src/components/Hero.jsx
+++ my-portfolio/src/components/Hero.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Hero from "../assets/Hero.png";
+import facebook from '../assets/facebook.png'
+import twitter from '../assets/twitter.png'
+import instagram from '../assets/instagram.png'
+import linkedin from '../assets/linkedin.png'
+import reactLogo from '../assets/React.png'
+import reduxLogo from '../assets/Redux.png'
+import tailwind from '../assets/Tailwind Css.png'
+
 
 const Hero = () => {
   return (
     <>
-    
+
     </>
   )
 }
