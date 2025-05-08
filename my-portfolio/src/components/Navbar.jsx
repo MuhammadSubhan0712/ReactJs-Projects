9c80e2aa19372c0f091309b73a1825a2cb017c98
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-05-09 01:00:04 +0500

    Working..

--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -1,26 +1,50 @@
-import React from 'react'
+import React, { useState } from "react";
 import logo from "../assets/logo.mp4";
+
 const Navbar = () => {
+  const [mobileMenuOpen, setmMobileMenuOpen] = useState(false);
+     
   return (
-    <header className='sticky top-0 z-50 w-full border-b bg-background/95 backdrop:blur support-[backdrop-filter]:bg-background/60 px-4 lg:px-0'>
-    <div className="max-w-7xl mx-auto flex h-14 items-center">
-      <div className="md:mr-4 justify-between w-full">
-        <a href="#" className='mr-6 flex items-center space-x-2'>
-           <video autoPlay loop muted>
-          <source src={logo}/>
-          </video>
-        </a>
-        <nav className='md:flex hidden items-center space-x-6 text-lg font-medium'>
-                <a href="#about" className='transition-colors hover:text-foreground/80 text-foreground/60'>About</a>
-                <a href="#projects" className='transition-colors hover:text-foreground/80 text-foreground/60'>Projects</a>
-                <a href="#testimonials" className='transition-colors hover:text-foreground/80 text-foreground/60'>Testimonials</a>
-                <a href="#contact" className='transition-colors hover:text-foreground/80 text-foreground/60'>Contact</a>
-        </nav>
+
+    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop:blur support-[backdrop-filter]:bg-background/60 px-4 lg:px-0">
+      <div className="max-w-7xl mx-auto flex h-14 items-center">
+        <div className="md:mr-4 justify-between w-full">
+          <a href="#" className="mr-6 flex items-center space-x-2">
+            <video autoPlay loop muted>
+              <source src={logo} />
+            </video>
+          </a>
+          <nav className="md:flex hidden items-center space-x-6 text-lg font-medium">
+            <a
+              href="#about"
+              className="transition-colors hover:text-foreground/80 text-foreground/60">
+              About
+            </a>
+            <a
+              href="#projects"
+              className="transition-colors hover:text-foreground/80 text-foreground/60">
+              Projects
+            </a>
+            <a
+              href="#contact"
+              className="transition-colors hover:text-foreground/80 text-foreground/60">
+              Contact
+            </a>
+          </nav>
+        </div>
+
+        {/* For mobile menu */}
+        <button className='inline-flex items-center justify-center rounded-md md:hidden' onClick={()=>setmMobileMenuOpen(!mobileMenuOpen)}>
+           <span className='sr-only'>Open main menu</span>
+           {mobileMenuOpen ? (
+            <X className='h-6 w-6' aria-hidden="true"/>
+           ):(
+            <Menu className='h-6 w-6' aria-hidden="true"/>
+           )}
+        </button>
       </div>
-    </div>
-
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
