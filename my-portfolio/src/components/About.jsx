3a0a550d10c4143e5b15eecd4bcff295580b4bdd
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-05-10 00:34:34 +0500

    Working..

--- my-portfolio/src/components/About.jsx
+++ my-portfolio/src/components/About.jsx
@@ -7,7 +7,11 @@ const About = () => {
         <div className="bg-gray-100 py-12">
             <div className="max-w-7xl mx-auto px-4 sm:px6 lg:px-8">
                 <div className="text-center">
-                    
+                <h2 className='text-base text-red-600 font-semibold tracking-wide uppercase'>About Me</h2>
+                    <p className='mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl'>Hi, I'm Muhammad Subhan Khan</p>
+                        <p className='mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto'>
+                            A Full-Stack Developer with expertise in web development, WordPress, SEO, and the MERN stack.
+                        </p>   
                 </div>
             </div>
         </div>
