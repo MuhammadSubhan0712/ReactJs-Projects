cd5b45d612ada8018fe0f5a1f224be6bef5bb53a
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-05-10 00:31:48 +0500

    Working..

--- my-portfolio/src/components/About.jsx
+++ my-portfolio/src/components/About.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+    <div className="relative" id='about'>
+        <div className="bg-gray-100 py-12">
+            <div className="max-w-7xl mx-auto px-4 sm:px6 lg:px-8">
+                <div className="text-center">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+    </>
   )
 }
 
